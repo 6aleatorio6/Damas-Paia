e71041258b030e7845d84650121d70937f4f00a9
leo <leonardolfelix12@gmail.com>
2024-02-04 21:50:51 -0300

    footer e header criados

---
+++ server/src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import { Container, Row, Col, Image } from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <footer className="mt-5 bg-dark-subtle   ">
+      <div
+        style={{
+          backgroundColor: "#8358ae",
+          padding: "2px 15px 5px 0",
+        }}
+      >
+        <Container className=" d-flex flex-row-reverse ">
+          <a href="https://github.com/6aleatorio6/Damas-Paia">
+            <Image
+              src="/imgs/github-mark-white.svg"
+              width={20}
+              height={20}
+              alt="github icon"
+            />
+          </a>
+        </Container>
+      </div>
+      <Container>
+        <Row className="text-center">
+          <p className=" my-2">
+            &copy; 2024 DamasPaias. Todos os direitos reservados.
+          </p>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
---
+++ server/src/components/Header.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { Button, Container, Nav,  Navbar } from "react-bootstrap";
+
+export default function Header() {
+  return (
+    <header>
+      <Navbar className="bg-dark-subtle">
+        <Container>
+          <Navbar.Brand  href="#home">Damas Paias</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            {false ? (
+              <Navbar.Text>
+                conta: <a href="#login">Paia cabral#0123</a>
+              </Navbar.Text>
+            ) : (
+              <Nav>
+                <Nav.Link href="/">Entrar</Nav.Link>
+                <Button variant="primary" href="/">Cadastrar</Button>
+              </Nav>
+            )}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+ 
