8d66786e56f60130f80f21b750fdd936aaed4356
leo <leonardolfelix12@gmail.com>
2024-02-18 22:41:22 -0300

    delete resource tabu

--- sv_backend/src/tabu/dto/update-tabu.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTabuDto } from './create-tabu.dto';
-
-export class UpdateTabuDto extends PartialType(CreateTabuDto) {}
--- sv_backend/src/tabu/tabu.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TabuService } from './tabu.service';
-import { TabuController } from './tabu.controller';
-
-@Module({
-  controllers: [TabuController],
-  providers: [TabuService],
-})
-export class TabuModule {}
