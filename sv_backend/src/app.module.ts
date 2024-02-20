d40c8c5d0fbac5e3d249fa4e1b146d2ab8b537a5
leo <leonardolfelix12@gmail.com>
2024-02-20 19:12:09 -0300

    tabu removed

--- sv_backend/src/app.module.ts
+++ sv_backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TabuModule } from './tabu/tabu.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TabuModule, UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
