4c2acd82d24db057f3de8d3d68785ec5a39e8378
leo <leonardolfelix12@gmail.com>
2024-02-24 21:02:43 -0300

    feat: add helper "tentarDnvSeDerErro"

--- backend/src/shared/helpers.service.ts
+++ backend/src/shared/helpers.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class HelpersShared {
-  private get numeroAleatorio(): number {
+  get numeroAleatorio(): number {
     return Math.floor(Math.random() * 10000);
+  }
+
+  async gerarHashSenha(senha: string): Promise<string> {
+    return await bcrypt.hash(senha, 13);
+  }
+
+  async tentarDnvSeDerErro<T>(cb: () => T, tentarQntsVezes = 5): Promise<T> {
+    for (let i = 0; i < tentarQntsVezes; i++) {
+      try {
+        return await cb();
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 }
